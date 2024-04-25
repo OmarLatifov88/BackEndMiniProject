1ed704b85250253731b4452bdbfad4f6e78fd2fc
OmarLatifov88 <omarnl@code.edu.az>
2024-04-25 18:00:26 +0400

    change

---
+++ Backend Project/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Project.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Categories()
+        {
+            return View();
+        }
+    }
+}
